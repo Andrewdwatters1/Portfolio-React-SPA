f81fbec5245a7081b17412c5515e21855122648d
Andrew Watters <Andrewdwatters1@gmail.com>
2018-09-30 18:47:16 -0600

    Production prep

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+require('dotenv').config();
+const path = require('path')
+
+const app = express();
+const serverPort = process.env.SERVER_PORT;
+app.use(bodyParser.json());
+app.use( express.static( `${__dirname}/../build` ) );
+
+app.get('*', (req, res) => { // PRODUCTION BUILD ONLY
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+app.listen(serverPort, () => {
+  console.log('Backend port: ', serverPort);
+})
