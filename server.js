0b7c71e74885ec300005fc96d6e81c95204e108a
Andrew Watters <Andrewdwatters1@gmail.com>
2018-09-30 19:22:12 -0600

    updated filepath to build folder, server at root

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use( express.static( `${__dirname}/../build` ) );
 
 app.get('*', (req, res) => { // PRODUCTION BUILD ONLY
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+  res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
 app.listen(serverPort, () => {
